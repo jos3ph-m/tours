9ac023e0413150f7f211f3f0ec761b69735c9e0c
Joseph M <jmedina1985@hotmail.com>
2021-03-08 22:15:10 -0800

    get the list of tours

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ const url = 'https://course-api.com/react-tours-project';
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+  };
+
   if (loading) {
     return (
       <main>
