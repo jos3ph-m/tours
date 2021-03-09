d5956b6a0928aad07e1f8c7a24e994ae57ce8881
Joseph M <jmedina1985@hotmail.com>
2021-03-08 22:12:29 -0800

    If loading show loading component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ const url = 'https://course-api.com/react-tours-project';
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return <h2>Tours Project Setup</h2>;
 }
 
