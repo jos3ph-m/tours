d146bd590eed0a6de8231194aefa058bad59456b
Joseph M <jmedina1985@hotmail.com>
2021-03-08 22:36:21 -0800

    get the list of tours

--- src/App.js
+++ src/App.js
@@ -11,8 +11,16 @@ function App() {
 
   const fetchTours = async () => {
     setLoading(true);
-    const response = await fetch(url);
-    const tours = await response.json();
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   };
   useEffect(() => {
     fetchTours();
