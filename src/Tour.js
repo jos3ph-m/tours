343ee3eb514b617d410cc421a6a80a081078c8c0
Joseph M <jmedina1985@hotmail.com>
2021-03-08 22:42:36 -0800

    get the list of tours

--- src/Tour.js
+++ src/Tour.js
@@ -4,6 +4,12 @@ const Tour = ({ id, image, info, price, name }) => {
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+      </footer>
     </article>
   );
 };
