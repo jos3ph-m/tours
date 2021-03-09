2786af0d2e1abceb0b52e107ed8d2c6bf06bbe4a
Joseph M <jmedina1985@hotmail.com>
2021-03-08 22:11:08 -0800

    Getting set up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const url = 'https://course-api.com/react-tours-project';
 
 function App() {
   const [loading, setLoading] = useState(true);
-
+  const [tours, setTours] = useState([]);
   return <h2>Tours Project Setup</h2>;
 }
 
