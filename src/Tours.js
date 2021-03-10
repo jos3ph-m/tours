93207c30fbf9b6d7be9d57f84f0b84b6282e4ed4
Joseph M <jmedina1985@hotmail.com>
2021-03-09 20:40:26 -0800

    remove tour functionality

--- src/Tours.js
+++ src/Tours.js
@@ -9,7 +9,7 @@ const Tours = ({ tours, removeTour }) => {
       </div>
       <div>
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour}></Tour>;
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
         })}
       </div>
     </section>
